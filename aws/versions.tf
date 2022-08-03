992a9994515addc741a641494b1643b41ac1ad28
Pedro Belem <pedrocb_c-s@hotmail.com>
2022-08-03 22:08:43 +0100

    Add organization

---
+++ aws/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket="terraform-20220803205515755700000001"
+    key="root"
+    region="eu-west-1"
+    profile="root-account"
+  }
+}
