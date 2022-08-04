c65ab4b5c088fee6e31a6c3aa8a4dae6f9ede168
Pedro Belem <pedrocb_c-s@hotmail.com>
2022-08-04 22:35:31 +0100

    Add movie releases account

--- aws/organization.tf
+++ aws/organization.tf
@@ -1,1 +1,21 @@
 resource "aws_organizations_organization" "org" {}
+
+resource "aws_organizations_account" "this" {
+  for_each = var.accounts
+  name = each.key
+  email = "${each.key}+pbelem1995@gmail.com"
+
+}
+
+module "movie-releases_state_backend" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+
+  providers = {
+    aws = aws.movie-releases
+  }
+}
--- aws/provider.tf
+++ aws/provider.tf
@@ -3,3 +3,12 @@ provider "aws" {
   allowed_account_ids = ["477988886247"]
   profile = "root-account"
 }
+provider "aws" {
+  profile = "root-account"
+  region = "eu-west-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::${aws_organizations_account.this["movie-releases"].id}:role/OrganizationAccountAccessRole"
+  }
+  alias = "movie-releases"
+}
